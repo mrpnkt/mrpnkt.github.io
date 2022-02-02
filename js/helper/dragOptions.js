4f804b9035fc9e076626b7dbacc01eb67092a1f0
mrpnkt <2102625+mrpnkt@users.noreply.github.com>
2022-02-02 09:15:13 +0100

    Revert "starttree upload"
    This reverts commit cafbda232ecc81c966581e9fbb2c7fd9592c6262.

--- js/helper/dragOptions.js
+++
@@ -1,6 +0,0 @@
-export default class DragOptions {
-  constructor({ data, validDropzones }) {
-    this.data = data ?? "";
-    this.validDropzones = validDropzones ?? []; // classes that this draggable can be dropped on
-  }
-}
